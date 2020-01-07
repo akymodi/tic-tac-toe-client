db4fd7efd2c170b1aae83ecee7df17653408b48c
Akash Modi <expese@Akashs-MacBook-Pro.local>
2020-01-07 11:45:13 -0500

    Add X in boxes on click
    Got X in boxes to work now needs to work on O

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,11 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+const clickEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   authEvents.addHandlers()
+  clickEvents.addHandlers()
 })
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,9 +1,11 @@
 'use strict'
 
+// links to other folder
 const api = require('./api')
 const ui = require('./ui')
 const getFormFields = require('./../../../lib/get-form-fields')
 
+// function that allows user to sign up
 const onSignUp = () => {
   event.preventDefault()
   const form = event.target
@@ -12,10 +14,10 @@ const onSignUp = () => {
     .then(ui.signUpSuccess)
     .catch(ui.signUpFailure)
   // handle the API response
-  console.log(data)
+  // console.log(data)
 }
-//
 
+// function that allows user to sign in
 const onSignIn = () => {
   event.preventDefault()
   const form = event.target
@@ -23,9 +25,10 @@ const onSignIn = () => {
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
-  console.log(data)
+  // console.log(data)
 }
 
+// function that allows user to change password
 const onPwdChange = () => {
   event.preventDefault()
   const form = event.target
@@ -33,22 +36,36 @@ const onPwdChange = () => {
   api.changePwd(data)
     .then(ui.pWdChangeSuccess)
     .catch(ui.pWdChangeFailure)
-  console.log(data)
+  // console.log(data)
 }
 
+// function that allows user to sign out
 const onSignOut = () => {
   event.preventDefault()
-
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
 }
 
+// player logic
+const currentPlayer = 'X'
+const nextPlayer = 'O'
+
+const onClick = (event) => {
+  const id = event.target.id
+  console.log(id)
+  // id.innerHTML += ('x')
+  $(event.target).text('x')
+}
+
+// event handdler jquery
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#pwd-change').on('submit', onPwdChange)
   $('#sign-out').on('submit', onSignOut)
+  $('.col').on('click', onClick)
+  // $('#box-0').on('click', onClick2)
 }
 
 module.exports = {
