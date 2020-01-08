f0c43aa00033ecbf30197525ebcae459720680a8
Akash Modi <expese@akashs-mbp.lan>
2020-01-08 14:38:29 -0500

    Add gameBoard folder
    Added gameBoard folder for better readability and organizing codes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,13 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+const gameEvents = require('./gameBoard/events')
 // const clickEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   authEvents.addHandlers()
+  gameEvents.addHandlers()
   // clickEvents.addHandlers()
 })
