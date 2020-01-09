cd96eb3b11120f135c1f853cf889b4152f15703f
Akash Modi <expese@Akashs-MacBook-Pro.local>
2020-01-09 15:30:49 -0500

    Add game over logic
    Added game over logic that prevents from continue playing after the game
    is over

--- assets/scripts/gameBoard/events.js
+++ assets/scripts/gameBoard/events.js
@@ -5,7 +5,7 @@ const ui = require('./ui')
 const getFormFields = require('./../../../lib/get-form-fields')
 
 // variable for game start or finish
-const gameOver = false
+let gameOver = false
 
 // create a veriable  arrays ofempty strings to use
 const gameBoard = ['', '', '', '', '', '', '', '', '']
@@ -21,59 +21,71 @@ let currentPlayer = 'X'
 const onClick = (event) => {
   event.preventDefault()
 
+  if (!gameOver) {
   // getting the ID of the box clicked
-  const id = event.target.id
-  console.log(id)
+    const id = event.target.id
+    console.log(id)
 
-  // this put current player value on the board
-  $(event.target).text(currentPlayer)
+    // this put current player value on the board
+    $(event.target).text(currentPlayer)
 
-  // getting the value from the current value and passing it on to the empty arrays
-  gameBoard[id] = currentPlayer
-  console.log(gameBoard)
+    // getting the value from the current value and passing it on to the empty arrays
+    gameBoard[id] = currentPlayer
+    console.log(gameBoard)
 
-  // ternery operator that switches the player turn
-  currentPlayer === 'X' ? currentPlayer = 'O' : currentPlayer = 'X'
+    // ternery operator that switches the player turn
+    currentPlayer === 'X' ? currentPlayer = 'O' : currentPlayer = 'X'
+  } else {
+    console.log('Good Luck')
+  }
+  // callback checkWinning function to see if the game is over or not
   checkWining()
 }
 
 const checkWining = () => {
   // if top row is a win
   if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
+    gameOver = true
     console.log('You Win')
     // else if middle row is a win
   } else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
+    gameOver = true
     console.log('You Win')
   // else if bottom row is a win
   } else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
+    gameOver = true
     console.log('You Win')
   // else if left column is a win
   } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
+    gameOver = true
     console.log('You Win')
   // else if middle aolumn is a win
   } else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
+    gameOver = true
     console.log('You Win')
   // else if right column is a win
   } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
+    gameOver = true
     console.log('You Win')
   // else if right diagonal is a win
   } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+    gameOver = true
     console.log('You Win')
   // else if left diagonal is a win
   } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
+    gameOver = true
     console.log('You Win')
   // default statement
   } else {
+    gameOver = false
     console.log('Game not over yet')
   }
 }
 
-// const
-
 const addHandlers = () => {
   $('.box').one('click', onClick)
+  // $('.game-header').on('result', gameResult)
 }
-
 module.exports = {
   addHandlers
 }
